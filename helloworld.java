a2ddc969ae2d72a1add851a49975f7ac6b95fb90
hemasri026royal <hemasri026royal@gmail.com>
2025-11-30 12:28:56 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
-public class Helloworld {
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
+public class helloworld {
+    public static void main(String[] args){
+        System.out.println("Hello World");
     }
 }
